41977ec60411c41be8de649f869cb6ae103aa5a6
Muskan Gupta <37034522+muskan28@users.noreply.github.com>
2018-03-04 11:56:24 +0530

    Add files via upload

---
+++ hanoi.py
@@ -0,0 +1,13 @@
+#Program to implement toer of hanoi
+
+def hanoi(n,A,B,C):
+	if n>0:
+		hanoi(n-1,A,C,B)
+		if A:
+			C.append(A.pop())
+		hanoi(n-1,B,A,C)
+A=[1,2,3,4]
+C=[]
+B=[]
+hanoi(len(A),A,B,C)
+print(A,B,C)		
